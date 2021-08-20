16fcba74251eb2a5626acf8b5bc9ad747c31164b
Albert O'Connor <albert@albertoconnor.ca>
2021-08-20 16:01:05 -0400

    Actually fix it

--- itchymws/utils.py
+++ itchymws/utils.py
@@ -20,7 +20,7 @@ def calculate_signature(request, secret):
     request should be a list containing strings that represent each line above
     """
     key = "\n".join(request).encode('utf-8')
-    signature = hmac.new(secret, "\n".join(request), hashlib.sha256).digest()
+    signature = hmac.new(secret, key, hashlib.sha256).digest()
     return base64.b64encode(signature)
 
 
